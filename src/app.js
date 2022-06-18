1e01384d52a635ba0a0607bf42559622157c8aa9
ishilbi <107364357+ishilbi@users.noreply.github.com>
2022-06-17 22:45:22 -0400

    added axios script

---
+++ src/app.js
@@ -0,0 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+
+  let humidityElement = document.querySelector("#humidity-level");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "f3a66372ec0940e078d8212f937f2ff1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
